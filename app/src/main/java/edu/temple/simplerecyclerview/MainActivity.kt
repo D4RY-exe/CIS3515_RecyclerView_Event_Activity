65b0eacc7ec44cdc6d164abaa4a3942d7ff3d934
D4RY-exe <129776733+D4RY-exe@users.noreply.github.com>
2025-02-13 12:12:30 -0500

    In-Class coding, practice with using adapters

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,8 +2,10 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +17,20 @@ class MainActivity : AppCompatActivity() {
         val numbers = Array(100) {it + 1}
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val displayTextView = findViewById<TextView>(R.id.displayTextView)
-        recyclerView.layoutManager = GridLayoutManager(this, 4)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
 
+        val callBack = {size : Int ->
+            displayTextView.textSize = size.toFloat()
+        }
+
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
+        recyclerView.adapter = NumberDisplayAdapter(numbers){size : Int ->
+            displayTextView.textSize = size.toFloat()
+        }
+
+        //val viewHolder = NumberDisplayAdapter(numbers).NumberViewHolder(TextView(unfinished))
 
 
     }
